d54d5b5ed7e59f26d46ddc0ba9835d62864fb623
tavisca-vjola <51903550+tavisca-vjola@users.noreply.github.com>
2019-10-09 10:58:26 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -23,6 +23,7 @@ namespace web_hook
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>();/////////////////////
+        ////////////////////////////
     }
 }
